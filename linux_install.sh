82ed72ff339fa52425fb095f4104902b396a7932
Bryce Harrington <bryce@bryceharrington.org>
2015-09-10 22:25:44 -0700

    install: Fix installation paths

--- linux_install.sh
+++ linux_install.sh
@@ -6,12 +6,15 @@ SBDF9_BACKUP=~/.local/share/doublefine/spacebasedf9/Saves/
 ## Backup if needed
 if [ ! -e ${STEAM_COMMON}/SpacebaseDF9.v1 ]; then
     echo "Backing up original SpacebaseDF9 code and game save"
-    rsync -avz ${STEAM_COMMON}/SpacebaseDF9 ${STEAM_COMMON}/SpacebaseDF9.v1
-    rsync -avz ${SBDF9_BACKUP}/SpacebaseDF9AutoSave.sav ${SBDF9_BACKUP}/SpacebaseDF9AutoSave-v1.sav 
+    rsync -avz ${STEAM_COMMON}/SpacebaseDF9/ ${STEAM_COMMON}/SpacebaseDF9.v1
+fi
+if [ ! -e ${SBDF9_BACKUP}/Archives/SpacebaseDF9AutoSave-v1.sav ]; then
+    mkdir -p ${SBDF9_BACKUP}/Archives
+    rsync -avz ${SBDF9_BACKUP}/SpacebaseDF9AutoSave.sav ${SBDF9_BACKUP}/Archives/SpacebaseDF9AutoSave-v1.sav 
 fi
 
-rsync -avz Dialog ${STEAM_COMMON}/SpacebaseDF9/
-rsync -avz Scripts ${STEAM_COMMON}/SpacebaseDF9/
-rsync -avz UILayouts ${STEAM_COMMON}/SpacebaseDF9/
+rsync -avz Dialog ${STEAM_COMMON}/SpacebaseDF9/Data/
+rsync -avz Scripts ${STEAM_COMMON}/SpacebaseDF9/Data/
+rsync -avz UILayouts ${STEAM_COMMON}/SpacebaseDF9/Data/
 
 
