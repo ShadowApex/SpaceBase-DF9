927095285ccf5f64cb9e9ee4a4362a6186eedbf3
Skenners <bobtrusty1@gmail.com>
2015-09-12 18:22:08 +1000

    O2Gen4 is complete-ish. gotta fix the little gold halo that goes around it, but its a perfectly playable piece of hardware, just like the o2gen3. Use bahns debug menu to get through all the research on the o2gens then give it a build!

--- Win/Munged/Environments/O2Gen4.lua
+++ Win/Munged/Environments/O2Gen4.lua
@@ -3,28 +3,28 @@ return {
     frames = {
             {
                 name = "O2Gen4",
-                spriteColorRect = { x = 0, y = 0, width = 320, height = 356 },
-                uvRect = { u0 = 0.0, v0 = 0.0, u1 = 0.3125, v1 = 1.0 },
-                spriteSourceSize = { width = 320, height = 256 },
+                spriteColorRect = { x = 0, y = 0, width = 389, height = 320 },
+                uvRect = { u0 = 0.0625, v0 = 0.125, u1 = 0.4423, v1 = 0.375 },
+                spriteSourceSize = { width = 389, height = 320 },
                 spriteTrimmed = false,
                 textureRotated = false
             },
             {
                 name = "O2Gen4_damaged",
-				spriteColorRect = { x = 0, y = 0, width = 320, height = 356 },
-                uvRect = { u0 = 0.3333, v0 = 0.0, u1 = 0.6455, v1 = 1.0 },
-                spriteSourceSize = { width = 320, height = 256 },
+                spriteColorRect = { x = 0, y = 0, width = 389, height = 320 },
+                uvRect = { u0 = 0.5625, v0 = 0.125, u1 = 0.9423, v1 = 0.375 },
+                spriteSourceSize = { width = 389, height = 320 },
                 spriteTrimmed = false,
                 textureRotated = false
             },
             {
                 name = "O2Gen4_destroyed",
-                spriteColorRect = { x = 0, y = 0, width = 320, height = 356 },
-                uvRect = { u0 = 0.6666, v0 = 0.0, u1 = 0.9785, v1 = 1.0 },
-                spriteSourceSize = { width = 320, height = 256 },
+                spriteColorRect = { x = 0, y = 0, width = 389, height = 320 },
+                uvRect = { u0 = 0.0625, v0 = 0.625, u1 = 0.4423, v1 = 0.875 },
+                spriteSourceSize = { width = 389, height = 320 },
                 spriteTrimmed = false,
                 textureRotated = false
             },
         }
-	}
+    } 
     
