cf8fa08d4412fcfceef997f1c7ddf9b80ecb299b
Skenners <bobtrusty1@gmail.com>
2015-09-26 16:25:18 +1000

    Final 1.07 upload. everything after will be 1.08. Congrats!

--- Win/Munged/Environments/Jukebox.lua
+++ Win/Munged/Environments/Jukebox.lua
@@ -3,25 +3,25 @@ return {
     frames = {
             {
                 name = "Juke",
-                spriteColorRect = { x =-15, y = -120, width = 88, height = 140 },
-                uvRect = { u0 = 0.0, v0 = 0.0, u1 = 0.333, v1 = 1 },
-                spriteSourceSize = { width = 85, height = 40 },
+                spriteColorRect = { x = -10, y = -10, width = 103, height = 150 },
+				uvRect = { u0 = 0.0, v0 = 0.0, u1 = 0.333, v1 = 0.577 },
+				spriteSourceSize = { width = 103, height = 130 },
                 spriteTrimmed = true,
                 textureRotated = false
             },
             {
                 name = "Juke_damaged",
-                spriteColorRect = { x =5, y = -120, width = 88, height = 140 },
-                uvRect = { u0 = 0.333, v0 = 0.0, u1 = 0.666, v1 = 1 },
-                spriteSourceSize = { width = 88, height = 40 },
+				spriteColorRect = { x = -10, y = -10, width = 103, height = 150 },
+				uvRect = { u0 = 0.333, v0 = 0.0, u1 = 0.666, v1 = 0.577 },
+				spriteSourceSize = { width = 103, height = 130 },
                 spriteTrimmed = true,
                 textureRotated = false
             },
             {
                 name = "Juke_destroyed",
-                spriteColorRect = { x =5, y = -120, width = 88, height = 140 },
-                uvRect = { u0 = 0.666, v0 = 0.0, u1 = 1, v1 = 1 },
-                spriteSourceSize = { width = 88, height = 40 },
+				spriteColorRect = { x = -10, y = -10, width = 103, height = 150 },
+				uvRect = { u0 = 0.666, v0 = 0.0, u1 = 1.0, v1 = 0.577 },
+				spriteSourceSize = { width = 103, height = 130 },
                 spriteTrimmed = true,
                 textureRotated = false
             },
