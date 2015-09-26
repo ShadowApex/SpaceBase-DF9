931fa8e3fde3e4621c281a61c180ad204d1f7d8e
Skenners <bobtrusty1@gmail.com>
2015-09-26 10:53:10 +1000

    Jukebox, Reactor 3 and 4 and some minor code changes.

---
+++ Win/Munged/Environments/Jukebox.lua
@@ -0,0 +1,29 @@
+return {
+    texture = 'Jukebox.png',
+    frames = {
+            {
+                name = "Juke",
+                spriteColorRect = { x =-15, y = -120, width = 88, height = 140 },
+                uvRect = { u0 = 0.0, v0 = 0.0, u1 = 0.333, v1 = 1 },
+                spriteSourceSize = { width = 85, height = 40 },
+                spriteTrimmed = true,
+                textureRotated = false
+            },
+            {
+                name = "Juke_damaged",
+                spriteColorRect = { x =5, y = -120, width = 88, height = 140 },
+                uvRect = { u0 = 0.333, v0 = 0.0, u1 = 0.666, v1 = 1 },
+                spriteSourceSize = { width = 88, height = 40 },
+                spriteTrimmed = true,
+                textureRotated = false
+            },
+            {
+                name = "Juke_destroyed",
+                spriteColorRect = { x =5, y = -120, width = 88, height = 140 },
+                uvRect = { u0 = 0.666, v0 = 0.0, u1 = 1, v1 = 1 },
+                spriteSourceSize = { width = 88, height = 40 },
+                spriteTrimmed = true,
+                textureRotated = false
+            },
+        }
+    } 
---
+++ Win/Munged/Environments/ReactorGen3.lua
@@ -0,0 +1,29 @@
+return {
+    texture = 'ReactorGen3.png',
+    frames = {
+            {
+                name = "ReactorGen3",
+				spriteColorRect = { x =27, y = 32, width = 341, height = 341 },
+                uvRect = { u0 = 0.0, v0 = 0.0, u1 = 0.333, v1 = 0.666 },
+                spriteSourceSize = { width = 341, height = 341 },
+                spriteTrimmed = true,
+                textureRotated = false
+            },
+            {
+                name = "ReactorGen3_damaged",
+				spriteColorRect = { x =27, y = -32, width = 341, height = 341 },
+                uvRect = { u0 = 0.333, v0 = 0.0, u1 = 0.666, v1 = 0.666 },
+                spriteSourceSize = { width = 341, height = 341 },
+                spriteTrimmed = true,
+                textureRotated = false
+            },
+            {
+                name = "ReactorGen3_destroyed",
+				spriteColorRect = { x =27, y = -32, width = 341, height = 341 },
+                uvRect = { u0 = 0.666, v0 = 0.0, u1 = 1.0, v1 = 0.666 },
+                spriteSourceSize = { width = 341, height = 341 },
+                spriteTrimmed = true,
+                textureRotated = false
+            },
+        }
+    } 
