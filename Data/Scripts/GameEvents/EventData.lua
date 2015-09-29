6a339797398b1f2eb64edd97445b29276bcbb92c
Bryce Harrington <bryce@bryceharrington.org>
2015-09-28 21:27:39 -0700

    events: Fix issues with undefined events from forecasts in saved games
    The game includes a mechanism to pre-roll several upcoming events, which
    it persists into the game save file (not sure why... anti save game
    cheating maybe?)  A recent commit dropped a number of older events,
    which can result in saved files referencing event indexes that no longer
    exist, leading to weird results and sometimes crashes.
    
    To fix (or at least sidestep) this, simply pad out our data structure
    with duplicated copies of the new events.  In time we'll replace these
    with more new content.

--- Data/Scripts/GameEvents/EventData.lua
+++ Data/Scripts/GameEvents/EventData.lua
@@ -41,6 +41,16 @@ local tEvents =
             rejectedResponse='DOCKUI237TEXT', rejectedResponseButton='DOCKUI238TEXT',
             screwYouResponse='DOCKUI237TEXT', screwYouResponseButton='DOCKUI238TEXT',
         },
+
+        -- FIXME: Duplicate event to take the place of the dropped DF event
+        {
+            title='DOCKUI232TEXT', request='DOCKUI233TEXT',
+            acceptButton='DOCKUI234TEXT', rejectButton='DOCKUI235TEXT',
+            -- can't avoid final siege with dialog, just different responses
+            acceptedResponse='DOCKUI236TEXT', acceptedResponseButton='DOCKUI238TEXT',
+            rejectedResponse='DOCKUI237TEXT', rejectedResponseButton='DOCKUI238TEXT',
+            screwYouResponse='DOCKUI237TEXT', screwYouResponseButton='DOCKUI238TEXT',
+        },
     },
 
     -- Brings 1-2 new crew members to the station unless population cap
@@ -88,11 +98,106 @@ local tEvents =
            rejectedResponse='DOCKUI218TEXT', rejectedResponseButton='DOCKUI219TEXT',
            screwYouResponse='DOCKUI220TEXT', screwYouResponseButton='DOCKUI221TEXT',
         },
+
+        -- FIXME: Duplicate events to take the place of the dropped DF events
+        {   -- kessel runner
+           title='DOCKUI152TEXT', request='DOCKUI153TEXT',
+           acceptButton='DOCKUI154TEXT', rejectButton='DOCKUI155TEXT',
+           acceptedResponse='DOCKUI156TEXT', acceptedResponseButton='DOCKUI157TEXT',
+           rejectedResponse='DOCKUI158TEXT', rejectedResponseButton='DOCKUI159TEXT',
+           screwYouResponse='DOCKUI160TEXT', screwYouResponseButton='DOCKUI161TEXT',
+        },
+        {   -- lister
+           title='DOCKUI162TEXT', request='DOCKUI163TEXT',
+           acceptButton='DOCKUI164TEXT', rejectButton='DOCKUI165TEXT',
+           acceptedResponse='DOCKUI166TEXT', acceptedResponseButton='DOCKUI167TEXT',
+           rejectedResponse='DOCKUI168TEXT', rejectedResponseButton='DOCKUI169TEXT',
+           screwYouResponse='DOCKUI170TEXT', screwYouResponseButton='DOCKUI171TEXT',
+        },
+        {   --holiday confusion
+           title='DOCKUI172TEXT', request='DOCKUI173TEXT',
+           acceptButton='DOCKUI174TEXT', rejectButton='DOCKUI175TEXT',
+           acceptedResponse='DOCKUI176TEXT', acceptedResponseButton='DOCKUI177TEXT',
+           rejectedResponse='DOCKUI178TEXT', rejectedResponseButton='DOCKUI179TEXT',
+           screwYouResponse='DOCKUI180TEXT', screwYouResponseButton='DOCKUI181TEXT',
+        },
+        {   --Dark Side of the Moon
+           title='DOCKUI182TEXT', request='DOCKUI183TEXT',
+           acceptButton='DOCKUI184TEXT', rejectButton='DOCKUI185TEXT',
+           acceptedResponse='DOCKUI186TEXT', acceptedResponseButton='DOCKUI187TEXT',
+           rejectedResponse='DOCKUI188TEXT', rejectedResponseButton='DOCKUI189TEXT',
+           screwYouResponse='DOCKUI190TEXT', screwYouResponseButton='DOCKUI191TEXT',
+        },
+        {   --Hitchhikers Guide to the galaxy reference
+           title='DOCKUI192TEXT', request='DOCKUI193TEXT',
+           acceptButton='DOCKUI194TEXT', rejectButton='DOCKUI195TEXT',
+           acceptedResponse='DOCKUI196TEXT', acceptedResponseButton='DOCKUI197TEXT',
+           rejectedResponse='DOCKUI198TEXT', rejectedResponseButton='DOCKUI199TEXT',
+           screwYouResponse='DOCKUI200TEXT', screwYouResponseButton='DOCKUI201TEXT',
+        },
+        {   --Bad thing happened
+           title='DOCKUI212TEXT', request='DOCKUI213TEXT',
+           acceptButton='DOCKUI214TEXT', rejectButton='DOCKUI215TEXT',
+           acceptedResponse='DOCKUI216TEXT', acceptedResponseButton='DOCKUI217TEXT',
+           rejectedResponse='DOCKUI218TEXT', rejectedResponseButton='DOCKUI219TEXT',
+           screwYouResponse='DOCKUI220TEXT', screwYouResponseButton='DOCKUI221TEXT',
+        },
+        {   -- kessel runner
+           title='DOCKUI152TEXT', request='DOCKUI153TEXT',
+           acceptButton='DOCKUI154TEXT', rejectButton='DOCKUI155TEXT',
+           acceptedResponse='DOCKUI156TEXT', acceptedResponseButton='DOCKUI157TEXT',
+           rejectedResponse='DOCKUI158TEXT', rejectedResponseButton='DOCKUI159TEXT',
+           screwYouResponse='DOCKUI160TEXT', screwYouResponseButton='DOCKUI161TEXT',
+        },
+        {   -- lister
+           title='DOCKUI162TEXT', request='DOCKUI163TEXT',
+           acceptButton='DOCKUI164TEXT', rejectButton='DOCKUI165TEXT',
+           acceptedResponse='DOCKUI166TEXT', acceptedResponseButton='DOCKUI167TEXT',
+           rejectedResponse='DOCKUI168TEXT', rejectedResponseButton='DOCKUI169TEXT',
+           screwYouResponse='DOCKUI170TEXT', screwYouResponseButton='DOCKUI171TEXT',
+        },
+        {   --holiday confusion
+           title='DOCKUI172TEXT', request='DOCKUI173TEXT',
+           acceptButton='DOCKUI174TEXT', rejectButton='DOCKUI175TEXT',
+           acceptedResponse='DOCKUI176TEXT', acceptedResponseButton='DOCKUI177TEXT',
+           rejectedResponse='DOCKUI178TEXT', rejectedResponseButton='DOCKUI179TEXT',
+           screwYouResponse='DOCKUI180TEXT', screwYouResponseButton='DOCKUI181TEXT',
+        },
+        {   --Dark Side of the Moon
+           title='DOCKUI182TEXT', request='DOCKUI183TEXT',
+           acceptButton='DOCKUI184TEXT', rejectButton='DOCKUI185TEXT',
+           acceptedResponse='DOCKUI186TEXT', acceptedResponseButton='DOCKUI187TEXT',
+           rejectedResponse='DOCKUI188TEXT', rejectedResponseButton='DOCKUI189TEXT',
+           screwYouResponse='DOCKUI190TEXT', screwYouResponseButton='DOCKUI191TEXT',
+        },
     },
 
     -- Ship drops off 1-2 raiders who attack crew and/or try to board station
     -- if the station has at least 6 crew members.
     hostileImmigrationEvents = {
+        {   --hero trap.
+           title='DOCKUI222TEXT', request='DOCKUI223TEXT',
+           acceptButton='DOCKUI224TEXT', rejectButton='DOCKUI225TEXT',
+           acceptedResponse='DOCKUI226TEXT', acceptedResponseButton='DOCKUI227TEXT',
+           rejectedResponse='DOCKUI228TEXT', rejectedResponseButton='DOCKUI229TEXT',
+           screwYouResponse='DOCKUI230TEXT', screwYouResponseButton='DOCKUI231TEXT',
+        },
+
+        -- FIXME: Duplicate events to take the place of the dropped DF events
+        {   --hero trap.
+           title='DOCKUI222TEXT', request='DOCKUI223TEXT',
+           acceptButton='DOCKUI224TEXT', rejectButton='DOCKUI225TEXT',
+           acceptedResponse='DOCKUI226TEXT', acceptedResponseButton='DOCKUI227TEXT',
+           rejectedResponse='DOCKUI228TEXT', rejectedResponseButton='DOCKUI229TEXT',
+           screwYouResponse='DOCKUI230TEXT', screwYouResponseButton='DOCKUI231TEXT',
+        },
+        {   --hero trap.
+           title='DOCKUI222TEXT', request='DOCKUI223TEXT',
+           acceptButton='DOCKUI224TEXT', rejectButton='DOCKUI225TEXT',
+           acceptedResponse='DOCKUI226TEXT', acceptedResponseButton='DOCKUI227TEXT',
+           rejectedResponse='DOCKUI228TEXT', rejectedResponseButton='DOCKUI229TEXT',
+           screwYouResponse='DOCKUI230TEXT', screwYouResponseButton='DOCKUI231TEXT',
+        },
         {   --hero trap.
            title='DOCKUI222TEXT', request='DOCKUI223TEXT',
            acceptButton='DOCKUI224TEXT', rejectButton='DOCKUI225TEXT',
@@ -113,6 +218,16 @@ local tEvents =
                 rejectedResponse='DOCKUI244TEXT', rejectedResponseButton='DOCKUI247TEXT',
                 screwYouResponse='DOCKUI245TEXT', screwYouResponseButton='DOCKUI248TEXT',
             },
+
+            -- FIXME: Duplicate events to take the place of the dropped DF events
+            {
+                -- Unknown delivery for Mr. Mumble
+                title='DOCKUI239TEXT', request='DOCKUI240TEXT',
+                acceptButton='DOCKUI241TEXT', rejectButton='DOCKUI242TEXT',
+                acceptedResponse='DOCKUI243TEXT', acceptedResponseButton='DOCKUI246TEXT',
+                rejectedResponse='DOCKUI244TEXT', rejectedResponseButton='DOCKUI247TEXT',
+                screwYouResponse='DOCKUI245TEXT', screwYouResponseButton='DOCKUI248TEXT',
+            },
         },
         hostile={
             {
@@ -123,7 +238,35 @@ local tEvents =
                 rejectedResponse='DOCKUI208TEXT', rejectedResponseButton='DOCKUI209TEXT',
                 screwYouResponse='DOCKUI210TEXT', screwYouResponseButton='DOCKUI211TEXT',
             },
+
+            -- FIXME: Duplicate events to take the place of the dropped DF events
+            {
+                --In the way of a hostile construction fleet
+                title='DOCKUI202TEXT', request='DOCKUI203TEXT',
+                acceptButton='DOCKUI204TEXT', rejectButton='DOCKUI205TEXT',
+                acceptedResponse='DOCKUI206TEXT', acceptedResponseButton='DOCKUI207TEXT',
+                rejectedResponse='DOCKUI208TEXT', rejectedResponseButton='DOCKUI209TEXT',
+                screwYouResponse='DOCKUI210TEXT', screwYouResponseButton='DOCKUI211TEXT',
+            },
+            {
+                --In the way of a hostile construction fleet
+                title='DOCKUI202TEXT', request='DOCKUI203TEXT',
+                acceptButton='DOCKUI204TEXT', rejectButton='DOCKUI205TEXT',
+                acceptedResponse='DOCKUI206TEXT', acceptedResponseButton='DOCKUI207TEXT',
+                rejectedResponse='DOCKUI208TEXT', rejectedResponseButton='DOCKUI209TEXT',
+                screwYouResponse='DOCKUI210TEXT', screwYouResponseButton='DOCKUI211TEXT',
+            },
         },
+        {
+            -- lottery winner
+            title='DOCKUI249TEXT', request='DOCKUI250TEXT',
+            acceptButton='DOCKUI251TEXT', rejectButton='DOCKUI252TEXT',
+            acceptedResponse='DOCKUI253TEXT', acceptedResponseButton='DOCKUI256TEXT',
+            rejectedResponse='DOCKUI254TEXT', rejectedResponseButton='DOCKUI257TEXT',
+            screwYouResponse='DOCKUI255TEXT', screwYouResponseButton='DOCKUI258TEXT',
+        },
+
+        -- FIXME: Duplicate events to take the place of the dropped DF events
         {
             -- lottery winner
             title='DOCKUI249TEXT', request='DOCKUI250TEXT',
