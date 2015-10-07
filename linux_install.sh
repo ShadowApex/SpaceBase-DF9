65409182d08390909ea29f2f1bcb2806ecedf798
Bryce Harrington <bryce@bryceharrington.org>
2015-10-06 23:43:58 -0700

    install: Fix install to paths with spaces in the name
    Fix suggested by BlackWidow.

--- linux_install.sh
+++ linux_install.sh
@@ -7,12 +7,12 @@ VERSION=1.07
 : ${SBDF9_SAVES:=~/.local/share/doublefine/spacebasedf9/Saves/}
 
 if [ "$1" = "restore" ]; then
-    if [ ! -d ${STEAM_COMMON}/SpacebaseDF9.v1 ]; then
+    if [ ! -d "${STEAM_COMMON}/SpacebaseDF9.v1" ]; then
 	echo "Missing original version of game to be restored"
 	echo "Try manually re-installing through Steam"
     else
-	rm -rf ${STEAM_COMMON}/SpacebaseDF9
-	mv ${STEAM_COMMON}/SpacebaseDF9.v1 ${STEAM_COMMON}/SpacebaseDF9
+	rm -rf "${STEAM_COMMON}/SpacebaseDF9"
+	mv "${STEAM_COMMON}/SpacebaseDF9.v1" "${STEAM_COMMON}/SpacebaseDF9"
     fi
     exit
 elif [ "$1" = "dist" ]; then
@@ -25,17 +25,17 @@ fi
 ## Backup if needed
 if [ ! -e ${STEAM_COMMON}/SpacebaseDF9.v1 ]; then
     echo "Backing up original SpacebaseDF9 code and game save"
-    rsync -avz ${STEAM_COMMON}/SpacebaseDF9/ ${STEAM_COMMON}/SpacebaseDF9.v1
+    rsync -avz "${STEAM_COMMON}/SpacebaseDF9/" "${STEAM_COMMON}/SpacebaseDF9.v1"
 fi
-if [ ! -e ${SBDF9_SAVES}/Archives/SpacebaseDF9AutoSave-v1.sav ]; then
-    mkdir -p ${SBDF9_SAVES}/Archives
-    rsync -avz ${SBDF9_SAVES}/SpacebaseDF9AutoSave.sav ${SBDF9_SAVES}/Archives/SpacebaseDF9AutoSave-v1.sav 
+if [ ! -e "${SBDF9_SAVES}/Archives/SpacebaseDF9AutoSave-v1.sav" ]; then
+    mkdir -p "${SBDF9_SAVES}/Archives"
+    rsync -avz "${SBDF9_SAVES}/SpacebaseDF9AutoSave.sav" "${SBDF9_SAVES}/Archives/SpacebaseDF9AutoSave-v1.sav"
 fi
 
-rsync -avz README ${STEAM_COMMON}/SpacebaseDF9/
+rsync -avz README "${STEAM_COMMON}/SpacebaseDF9/"
 
-rsync -avz Data/* ${STEAM_COMMON}/SpacebaseDF9/Data/
+rsync -avz Data/* "${STEAM_COMMON}/SpacebaseDF9/Data/"
 
 # Treat Win directory as authoritative for graphics
-rsync -avz Win/* ${STEAM_COMMON}/SpacebaseDF9/Linux/
+rsync -avz Win/* "${STEAM_COMMON}/SpacebaseDF9/Linux/"
 
